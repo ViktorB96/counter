91649b9cc24da4a50c43a20e586e5d88e1c3f9e4
Viktor Bašurovski <72989639+ViktorB96@users.noreply.github.com>
2021-01-30 19:38:17 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+    $(document).ready(function(){
+        setInterval(function(){
+            $("#time").load("time.php")
+        },1000)
+    })
+    </script>
+</head>
+    <title>Document</title>
+</head>
+<body>
+    <div id=time>
+    00 : 00 : 00 PM
+    </div>
+</body>
+</html>
